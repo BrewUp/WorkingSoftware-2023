672cbed5381bb6d4b7002b4c9240fd119e6c5109
Alberto Acerbis <alberto.acerbis@gmail.com>
2023-06-29 07:59:29 +0200

    Manage ChangePurchaseOrderStatusToComplete

---
+++ src/ModularEvent/BrewUp.Modules.Purchases.Domain/CommandHandlers/ChangePurchaseOrderStatusToCompleteCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using BrewUp.Modules.Purchases.Messages.Commands;
+using BrewUp.Modules.Purchases.Messages.Events;
+using BrewUp.Modules.Purchases.SharedKernel.Dtos;
+using MediatR;
+
+namespace BrewUp.Modules.Purchases.Domain.CommandHandlers;
+
+public class ChangePurchaseOrderStatusToCompleteCommandHandler : IRequestHandler<ChangePurchaseOrderStatusToComplete>
+{
+	private readonly IMediator _serviceBus;
+
+	public ChangePurchaseOrderStatusToCompleteCommandHandler(IMediator serviceBus)
+	{
+		_serviceBus = serviceBus;
+	}
+
+	public async Task Handle(ChangePurchaseOrderStatusToComplete request, CancellationToken cancellationToken)
+	{
+		// Do something
+		var purchaseOrderStatusCompleted = new PurchaseOrderStatusChangedToComplete(request.PurchaseOrderId, Enumerable.Empty<OrderLine>());
+		await _serviceBus.Publish(purchaseOrderStatusCompleted, cancellationToken);
+	}
+}
---
+++ src/ModularEvent/BrewUp.Modules.Purchases.ReadModel/EventHandlers/PurchaseOrderStatusChangedToCompleteEventHandler.cs
@@ -0,0 +1,13 @@
+﻿using BrewUp.Modules.Purchases.Messages.Events;
+using MediatR;
+
+namespace BrewUp.Modules.Purchases.ReadModel.EventHandlers;
+
+public class PurchaseOrderStatusChangedToCompleteEventHandler : INotificationHandler<PurchaseOrderStatusChangedToComplete>
+{
+	public Task Handle(PurchaseOrderStatusChangedToComplete notification, CancellationToken cancellationToken)
+	{
+		// Update the read model
+		return Task.CompletedTask;
+	}
+}
